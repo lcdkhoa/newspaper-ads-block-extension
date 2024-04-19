3f5412ba73ef21585c6b9dc80f19ff90fc423db6
Charlie Khoa <charlie.khoa@360f.com>
2024-04-19 21:44:56 +0700

    removed unused code

--- background.js
+++ background.js
@@ -1,13 +1,3 @@
-let extensionEnabled = true;
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.command == "toggleExtension") {
-    // Toggle the extension status
-    extensionEnabled = !extensionEnabled;
-    sendResponse({ status: "Extension toggled" });
-  }
-});
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
     chrome.scripting.executeScript({
